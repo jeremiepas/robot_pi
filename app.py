a82f991ddc3e622bd8df0e4157ec71e682f68860
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-28 16:47:40 +0200

    git test video robot

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from gevent import monkey
-monkey.patch_all()
-
 import cgi
 import redis
-from flask import Flask, render_template, request
+import cv2
+from flask import Flask, render_template, request, Response
 from flask_socketio import SocketIO
-from camera import VideoCamera
+
 import motors
+
 app = Flask(__name__, static_url_path='/static')
 db = redis.StrictRedis('localhost', 6379, 0)
 socketio = SocketIO(app)
@@ -25,15 +24,41 @@ def main():
 def pymeetups():
     return render_template('main.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def gen():
+    """Video streaming generator function."""
+    template = cv2.imread('moi.jpg',0)
+    w, h = template.shape[::-1]
+
+    meth = 'cv2.TM_CCOEFF_NORMED'
+    method = eval(meth)
+
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # Our operations on the frame come here
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        res = cv2.matchTemplate(img,template,method)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        # Display the resulting frame
+        top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(img,top_left, bottom_right, 255, 2)
+        cv2.imwrite('t.jpg', img)
+        yield (b'--frame\r\n'
+        b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @socketio.on('connect', namespace='/dd')
 def ws_conn():
