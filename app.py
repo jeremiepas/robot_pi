07d69b7277393820e58631c64de270c03e96f885
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-30 17:36:59 +0200

    mise a jour

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__, static_url_path='/static')
 db = redis.StrictRedis('localhost', 6379, 0)
 socketio = SocketIO(app)
 
+cap = cv2.VideoCapture(0)
+
 motorR = motors.Motor(27,17,1)
 motorL = motors.Motor(22,18,1)
 car = motors.Motorcar(motorL, motorR)
@@ -26,26 +28,11 @@ def pymeetups():
 
 def gen():
     """Video streaming generator function."""
-    template = cv2.imread('moi.jpg',0)
-    w, h = template.shape[::-1]
-
-    meth = 'cv2.TM_CCOEFF_NORMED'
-    method = eval(meth)
-
-    cap = cv2.VideoCapture(0)
-
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
-        # Our operations on the frame come here
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        res = cv2.matchTemplate(img,template,method)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        # Display the resulting frame
-        top_left = max_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-        cv2.rectangle(img,top_left, bottom_right, 255, 2)
-        cv2.imwrite('t.jpg', img)
+
+        cv2.imwrite('t.jpg', frame)
         yield (b'--frame\r\n'
         b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -78,7 +65,7 @@ def ws_city(message):
         car.forward(int(message['motorL']), int(message['motorR']))
     else:
         car.rearward(int(message['motorL']), int(message['motorR']))
-
+        pass
     # socketio.emit('motor', {'motor': cgi.escape(message['motor'])})
 
 if __name__ == '__main__':
